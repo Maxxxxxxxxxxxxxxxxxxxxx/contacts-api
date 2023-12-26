08f857ef919a487aaedde7817568ab855c268ce1
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2023-12-26 22:55:11 +0100

    update settings

--- ContactsAPI/Config/MongoSettings.cs
+++ ContactsAPI/Config/MongoSettings.cs
@@ -7,4 +7,17 @@ public class MongoSettings
     public string ContactCollection { get; set; } = null!;
     public string AccountCollection { get; set; } = null!;
     public string CredentialsCollection { get; set; } = null!;
+
+    public MongoSettings()
+    {
+    }
+
+    public MongoSettings(string connectionString)
+    {
+        ContactCollection = "contacts";
+        AccountCollection = "users";
+        CredentialsCollection = "credentials";
+        DatabaseName = "contactsApi";
+        ConnectionString = connectionString;
+    }
 }
