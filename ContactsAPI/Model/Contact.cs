4f9af5fd8e707fa7aeffd792f461798731e972df
Maxxxxxxxxxxxxxxxxxxxxx <m.schlamberger.674@studms.ug.edu.pl>
2023-12-26 22:56:26 +0100

    update contact schema

--- ContactsAPI/Model/Contact.cs
+++ ContactsAPI/Model/Contact.cs
@@ -10,10 +10,10 @@ public class Contact
     [BsonRepresentation(BsonType.ObjectId)]
     public string? Id { get; set; }
 
-    public string Name { get; set; }
-    public string Surname { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
-    public string DateOfBirth { get; set; }
-    public string PhoneNumber { get; set; }
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+    public string? DateOfBirth { get; set; }
+    public string? PhoneNumber { get; set; }
 }
